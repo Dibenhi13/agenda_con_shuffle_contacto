6a13d4d10b9f06647b91d515b22814802b1725d7
alumno <alumno@PC-V202-19.uacj.mx>
2025-02-26 10:46:00 -0700

    [MODIFICA] la pantalla agenda para usar el modelo nuevo

--- Agenda Shuffle/Agenda Shuffle/PantallaAgenda.swift
+++ Agenda Shuffle/Agenda Shuffle/PantallaAgenda.swift
@@ -5,13 +5,28 @@
 //  Created by alumno on 2/26/25.
 //
 
+
 import SwiftUI
+
+let contactos = [
+    ContactoAgenda(nombre: "Juan", telefono:"1232345"),
+    ContactoAgenda(nombre: "Juana", telefono:"1232345"),
+    ContactoAgenda(nombre: "Juan", telefono:"1232345"),
+    ContactoAgenda(nombre: "Juan", telefono:"1232345"),
+    ContactoAgenda(nombre: "Juan", telefono:"1232345"),
+    ContactoAgenda(nombre: "Juan", telefono:"1232345"),
+    ContactoAgenda(nombre: "Juan", telefono:"1232345"),
+    ContactoAgenda(nombre: "Juan", telefono:"1232345"),
+    ContactoAgenda(nombre: "Juan", telefono:"1232345")
+]
+
 
 struct PantallaAgenda: View {
     var body: some View {
         List{
             VStack {
-                ForEach(0...45, id: \.self){ _ in
+                ForEach(contactos){ contacto in
+                    Text("\(contacto.nombre)")
                     ContactoPrevista()
                 }
             }
@@ -20,6 +35,12 @@ struct PantallaAgenda: View {
         
     }
 }
+
+/*
+ modelo: MVC Controlador (iniciar sesion, login automatico)
+ modelo: modelo vista vistamodelo
+ modelo modelo vista (vista controlador modelo)
+ */
 
 #Preview {
     PantallaAgenda()
