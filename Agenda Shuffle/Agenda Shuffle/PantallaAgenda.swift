582af484b25a4c30b369e2fda07be5940f103114
alumno <alumno@PC-V202-19.uacj.mx>
2025-03-05 10:08:33 -0700

    [AGREGA] la funcionalidad e integra la pantalla agregar contacto

--- Agenda Shuffle/Agenda Shuffle/PantallaAgenda.swift
+++ Agenda Shuffle/Agenda Shuffle/PantallaAgenda.swift
@@ -22,15 +22,21 @@ struct PantallaAgenda: View {
     var largo_de_pantalla = UIScreen.main.bounds.width
     var ancho_de_pantalla = UIScreen.main.bounds.height
     
+    @State var mostrar_pantalla_agregar_contacto: Bool = false
+    
+    @State var contactos_actuales: [ContactoAgenda] = [
+        ContactoAgenda(nombre: "Joan", telefono:"52415"),
+        ContactoAgenda(nombre: "Joan", telefono:"52415"),
+        ContactoAgenda(nombre: "Joan", telefono:"52415"),
+        ContactoAgenda(nombre: "Joan", telefono:"52415"),
+        ContactoAgenda(nombre: "Joan", telefono:"52415"),
+        ContactoAgenda(nombre: "Joan", telefono:"52415"),
+    ]
     
     var body: some View {
         ScrollView{
             VStack (spacing: 10){
-                /*ForEach(0...15, id: \.self){_ in
-                    ContactoPrevista()
-                }*/
-                
-                ForEach(contactos){ contacto in
+                ForEach(contactos_actuales){ contacto in
                     //Text("\(contacto.nombre)")
                     ContactoPrevista(contacto_a_mostrar: contacto, al_pulsar:
                                         {print("Te envia saludos \(contacto.nombre) desde la pantalla agenda")})
@@ -56,12 +62,12 @@ struct PantallaAgenda: View {
                 
                 Image(systemName: "plus")
                     .background(Color.red)
-                    .offset(x: 0, y: -25)
             }
             
             .padding(15)
             .onTapGesture {
-                print("Falta implementar esta parte")
+                print("Falta implementar la seccion agregar contacto")
+                mostrar_pantalla_agregar_contacto.toggle()
             }
             
             Spacer()
@@ -85,6 +91,17 @@ struct PantallaAgenda: View {
                 print("Lanzar un intent para iniciar llamada")
             }
         
+        }
+        .background(Color.purple)
+        .sheet(isPresented: $mostrar_pantalla_agregar_contacto){
+            pantalla_agregar_contacto(boton_salir: {
+                mostrar_pantalla_agregar_contacto.toggle()
+            },
+             boton_agregar: {nombre, numero in
+                let contacto_nuevo = ContactoAgenda(nombre: nombre, telefono: numero)
+                contactos_actuales.append(contacto_nuevo)
+                mostrar_pantalla_agregar_contacto.toggle()
+            })
         }
     }
 }
--- Agenda Shuffle/Agenda Shuffle/Pantallas/pantalla_agregar_contacto.swift
+++ Agenda Shuffle/Agenda Shuffle/Pantallas/pantalla_agregar_contacto.swift
@@ -11,6 +11,15 @@ struct pantalla_agregar_contacto: View {
     @State private var nombre: String = ""
     @State private var numero_telefonico: String = ""
     
+    var boton_salir: () -> Void = {
+        print("Parece que te has equivocado")
+    }
+    
+    
+    var boton_agregar: (_ nombre : String, _ numero: String) -> Void = {nombre, numero in
+        print("Parece que te has equivocado")
+    }
+    
     
     var body: some View {
         Text("Colocar la etiqueta de nombre")
@@ -28,15 +37,20 @@ struct pantalla_agregar_contacto: View {
             .padding(10)
         
         HStack{
-            
-            Icono(tamano: 65, ruta_icono: "person.crop.circle.badge.plus", 
+            //Este icono es para agregar un contacto
+            Icono(tamano: 65, ruta_icono: "person.crop.circle.badge.plus",
                   padding: 10)
+            .onTapGesture {
+                boton_agregar(nombre, numero_telefonico)
+            }
             
             Spacer()
-            
+            //Este es para salir
             Icono(tamano: 65, ruta_icono: "return")
-                .background(nombre == "" ? Color.red:
-                                Color.cyan)
+                .background(nombre == "" ? Color.red: Color.cyan)
+                .onTapGesture {
+                    boton_salir()
+                }
 
         }
         .background(Color.cyan)
